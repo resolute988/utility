0a50aa9bc2616a5a0c8debf2979ffff164f250ec
vijay mathur <resolute988@gmail.com>
2021-06-01 05:26:33 +0000

    individual apis are ready to claimant and RP are implemented

--- Routes/sendEmailTo.js
+++ Routes/sendEmailTo.js
@@ -1,40 +1,16 @@
 const router= require("express").Router()
 const nodemailer= require("nodemailer");
 
-const sendEmail= (email,subject,html,feedback)=>{
-  
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASS
-    }
-  });
 
-  var mailOptions = {
-    from: 'resolute988@gmail.com',
-    to: obj.email,
-    subject: subject,
-    html: html 
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      res.status(200).send(feedback);
-    }
-  })
-}
-
-router.post("/",(req,res)=>{
-var feedback=[]
+router.post("/claimant",(req,res)=>{
     try { 
-        console.log("body",req.body)
-        const obj= req.body
-      
-      const claimant_subject=`Claim Filed Before NAMEXXXXXXX on DATE XXXX  `
-      const claimant_html=`Welcome to Resolute, <br/>
+      if(req.body.email!==undefined)
+      {
+      console.log("body",req.body)
+        const email= req.body.email
+   
+      const subject=`Claim Filed Before NAMEXXXXXXX on DATE XXXX claimant `
+      const html=`Welcome to Resolute, <br/>
       You claim has been successfully filed on datexxxxxxxx timexxxxxx before name xxxxxxxxxxxxxxxxx with the following attachments :  <br/>
       1. <br/> 
       2. <br/> 
@@ -48,27 +24,96 @@ var feedback=[]
       © Copyright 2021 DCirrus. All Rights Reserved. <br/>
       K 1/125 LGF, CR Park, New Delhi, 110019`
       
-        sendEmailTo(obj.claimant_email,subject,html,feedback)
-        const rp_subject=`Claim Filed Before NAMEXXXXXXX on DATE XXXX  `
-        const rp_html=`Welcome to Resolute, <br/>
-        You claim has been successfully filed on datexxxxxxxx timexxxxxx before name xxxxxxxxxxxxxxxxx with the following attachments :  <br/>
-        1. <br/> 
-        2. <br/> 
-        3. <br/> 
-        4. <br/> 
-        
-        Thanks <br/>
-        Team DCirrus <br/>
-        For any further assistance, please contact your support team at support@dcirrus.com <br/>
-        Do not respond to this email. This mailbox is not monitored. <br/>
-        © Copyright 2021 DCirrus. All Rights Reserved. <br/>
-        K 1/125 LGF, CR Park, New Delhi, 110019`
-       
-        sendEmailTo(obj.rp_email,subject,html,feedback)
-    } catch (error) {
+      
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS
+    }
+  });
+
+  var mailOptions = {
+    from: 'resolute988@gmail.com',
+    to: email,
+    subject:subject,
+    html: html 
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+     console.log("email sent to claimant =>",info)
+     res.status(200).send({response:true})
+   
+    }
+  })
+}else{
+  res.status(400).send({error:"no recipients defined"})
+}
+    
+      } catch (error) {
         res.status(400).send({err:error})
     }
   
+})
+
+router.post("/rp",(req,res)=>{
+  try { 
+    if(req.body.email!==undefined)
+    {
+    console.log("body",req.body)
+      const email= req.body.email
+ 
+    const subject=`Claim Filed Before NAMEXXXXXXX on DATE XXXX  RP`
+    const html=`Welcome to Resolute, <br/>
+    You claim has been successfully filed on datexxxxxxxx timexxxxxx before name xxxxxxxxxxxxxxxxx with the following attachments :  <br/>
+    1. <br/> 
+    2. <br/> 
+    3. <br/> 
+    4. <br/> 
+    
+    Thanks <br/>
+    Team DCirrus <br/>
+    For any further assistance, please contact your support team at support@dcirrus.com <br/>
+    Do not respond to this email. This mailbox is not monitored. <br/>
+    © Copyright 2021 DCirrus. All Rights Reserved. <br/>
+    K 1/125 LGF, CR Park, New Delhi, 110019`
+    
+    
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS
+  }
+});
+
+var mailOptions = {
+  from: 'resolute988@gmail.com',
+  to:email,
+  subject: subject,
+  html: html 
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+   console.log("email sent to RP =>",info)
+   res.status(200).send({response:true})
+ 
+  }
+})
+}else{
+res.status(400).send({error:"no recipients defined"})
+}
+  
+    } catch (error) {
+      res.status(400).send({err:error})
+  }
+
 })
 
 module.exports= router
