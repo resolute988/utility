984ea5ed8d6c04d78e80bca2622582ce44fc21d1
vijay mathur <resolute988@gmail.com>
2021-06-02 20:09:16 +0000

    export logs api is ready

--- Routes/exportLogs.js
+++ Routes/exportLogs.js
@@ -7,14 +7,14 @@ try{
     const {rp_id,rootFolderId}= req.body
     CreditorDetails.find(
         {rp_id,rootFolderId},
-        {creditor:1,first_name:1,last_name:1,email_id:1,phone_number:1,amount_claimed:1,amount_admitted:1,_id:0}
+        {creditor:1,first_name:1,last_name:1,email_id:1,phone_number:1,amount_claimed:1,amount_admitted:1,creditor_claim:1,_id:0}
         ).then(response=>{
         var d_response=[]
 
         response.map(obj=>{
-            var {creditor,first_name,last_name,email_id,phone_number,amount_claimed,amount_admitted}=obj
+            var {creditor,first_name,last_name,email_id,phone_number,amount_claimed,amount_admitted,creditor_claim}=obj
             first_name+=" "+last_name
-           d_response.push({creditor,first_name,email_id,phone_number,amount_claimed,amount_admitted}) 
+           d_response.push({'Co. Company' :creditor,'Name':first_name,'Email':email_id,'Mobile':phone_number,'Amount Claimed':amount_claimed,'Amount Admitted':amount_admitted,'Creditor Type':creditor_claim}) 
         })
 
         res.status(200).send({response:d_response})
